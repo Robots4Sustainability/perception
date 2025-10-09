3f065bf9bb40d45344412e96d63a358cce90aa11
Mohsin-Ali-Mirza <mohsinalimirxa@gmail.com>
2025-10-09 15:10:01 +0200

    Cleaned Repo

---
+++ models/readme.md
@@ -0,0 +1,1 @@
+Place the folder model into <your_ros_workspace>/
--- ros2_ws/build/perception/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/mohsin/official_build/ros2_ws/install/perception'
